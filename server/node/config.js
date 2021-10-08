2d22eb2bf836c3f604223e6eb6e67135940e863a
Mohit <mohitmadan71@gmail.com>
2021-10-08 21:07:36 +0530

    update config file

--- server/node/config.js
+++ server/node/config.js
@@ -15,7 +15,5 @@ module.exports = {
   app_url: process.env.ANUMATI_URL,
   client_api_key: process.env.CLIENT_API_KEY,
   rahasya_url: process.env.RAHASYA_URL,
-  // client_url: 'exp://192.168.1.12:19000'
-  client_url: 'http://127.0.0.1:19000',
-  backend_url: 'http://192.168.1.12:5000'
+  backend_url: process.env.BACKEND_URL
 };
